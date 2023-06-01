7989ef2c4108dcada33fdde1a13223cbf3c62e0a
Mehedi Hassan <mehedi.github@gmail.com>
2023-06-01 19:00:56 +0600

    filter design done

--- app/src/main/java/com/example/homepagesearchandfilter/ui/fragments/FilterFragment.kt
+++ app/src/main/java/com/example/homepagesearchandfilter/ui/fragments/FilterFragment.kt
@@ -7,15 +7,15 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.homepagesearchandfilter.R
 
+class FilterFragment : Fragment() {
 
-class MapFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false)
+        return inflater.inflate(R.layout.fragment_filter, container, false)
     }
 
 }
--- app/src/main/java/com/example/homepagesearchandfilter/ui/fragments/dialogs/MapDialogFragment.kt
+++ app/src/main/java/com/example/homepagesearchandfilter/ui/fragments/dialogs/MapDialogFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.example.homepagesearchandfilter.R
 import com.example.homepagesearchandfilter.databinding.FragmentMapDialogBinding
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -31,8 +32,14 @@ class MapDialogFragment : BottomSheetDialogFragment(), OnMapReadyCallback {
         val mapFragment = childFragmentManager
             .findFragmentById(R.id.googleMapId) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        binding.llSearch.setOnClickListener {
+
+            Navigation.findNavController(it).navigate(R.id.filterFragment)
+        }
         return binding.root
     }
+
 
 
     override fun onMapReady(googleMap: GoogleMap) {
