4b8d7dc57993f17c389cbb9fa16c7cdd8a3ba98c
Mehedi Hassan <mehedi.github@gmail.com>
2023-06-01 12:09:21 +0600

    home page done

--- app/src/main/java/com/example/homepagesearchandfilter/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/homepagesearchandfilter/ui/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.homepagesearchandfilter
+package com.example.homepagesearchandfilter.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.homepagesearchandfilter.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
