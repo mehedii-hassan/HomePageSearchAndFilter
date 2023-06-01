6efd087522c4356fc3fd5e7e1712ad062c332717
Mehedi Hassan <mehedi.github@gmail.com>
2023-06-01 12:51:09 +0600

    updated

--- app/src/main/java/com/example/homepagesearchandfilter/ui/fragments/HomePageFragment.kt
+++ app/src/main/java/com/example/homepagesearchandfilter/ui/fragments/HomePageFragment.kt
@@ -5,17 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.example.homepagesearchandfilter.R
+import com.example.homepagesearchandfilter.databinding.FragmentHomePageBinding
 
 class HomePageFragment : Fragment() {
 
+    private lateinit var binding: FragmentHomePageBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_page, container, false)
+    ): View {
+        //initialization
+        binding = FragmentHomePageBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.clLocation.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.selectLocationFragment)
+        }
+
+        binding.llSearch.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.mapFragment)
+        }
     }
 
 
